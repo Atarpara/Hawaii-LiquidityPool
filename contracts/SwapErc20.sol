9d054b83236e72b72af6cf1b54e6b19b0c090bd8
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2022-03-30 14:33:12 +0200

    feat: adding possibility yo buy tokens with eth

--- contracts/SwapErc20.sol
+++ contracts/SwapErc20.sol
@@ -10,6 +10,9 @@ contract SwapErc20 {
     IERC20 Honululu;
     address ownerHonululu;
 
+    // token price for ETH
+    uint256 public tokensPerEth = 100;
+
     //example: deploy VolcanoToken with ownerVulcano, after that approve the swap address using ownerVulcano in the VolcanoToken contract
     constructor(
         address _Volcano,
@@ -49,5 +52,43 @@ contract SwapErc20 {
     ) private {
         bool sent = token.transferFrom(sender, recipient, amount);
         require(sent, "token trasnfer failed");
+    }
+
+    function buyVolcano() public payable returns (uint256 tokenAmount) {
+        require(msg.value > 0, "Send ETH to buy some tokens");
+
+        uint256 amountToBuy = msg.value * tokensPerEth;
+
+        // check if the Vendor Contract has enough amount of tokens for the transaction
+        uint256 vendorBalance = Volcano.balanceOf(address(this));
+        require(
+            vendorBalance >= amountToBuy,
+            "Vendor contract has not enough tokens in its balance"
+        );
+
+        // Transfer token to the msg.sender
+        bool sent = Volcano.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer token to user");
+
+        return amountToBuy;
+    }
+
+    function buyHonululu() public payable returns (uint256 tokenAmount) {
+        require(msg.value > 0, "Send ETH to buy some tokens");
+
+        uint256 amountToBuy = msg.value * tokensPerEth;
+
+        // check if the Vendor Contract has enough amount of tokens for the transaction
+        uint256 vendorBalance = Honululu.balanceOf(address(this));
+        require(
+            vendorBalance >= amountToBuy,
+            "Vendor contract has not enough tokens in its balance"
+        );
+
+        // Transfer token to the msg.sender
+        bool sent = Volcano.transfer(msg.sender, amountToBuy);
+        require(sent, "Failed to transfer token to user");
+
+        return amountToBuy;
     }
 }
